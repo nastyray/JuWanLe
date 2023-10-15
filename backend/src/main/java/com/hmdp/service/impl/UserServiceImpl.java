d78d69f004a2f101cea9448d70baf010c4e1439c
lilray <2313715475@qq.com>
2023-10-15 09:38:31 +0800

    短信登录注册

--- backend/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.Random;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -25,6 +28,7 @@ import java.util.Random;
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    //发送验证码
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //1.校验手机号
@@ -40,5 +44,43 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功，验证码:{}",code);
         //返回ok
         return Result.ok();
+    }
+    //登录
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)){
+            //2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误!");
+        }
+        //校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)){
+            //3.不一致，报错
+            return Result.fail("验证码错误");
+        }
+
+        //4.一致，根据手机号查询用户，select * from tb_user  where phone = ?
+        User user = query().eq("phone", phone).one();
+        // 5. 判断用户是否存在
+        if (user == null){
+            //6.不存在，创建新用户并保存
+           user = createUserWithPhone(phone);
+        }
+        //7.保存用户到session中
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        //2.保存用户
+        save(user);
+        return user;
+
     }
 }
