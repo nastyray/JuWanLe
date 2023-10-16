c9fb4a82e0b6fcc12261ba08e4875c0da76e14ae
lilray <2313715475@qq.com>
2023-10-16 20:15:47 +0800

    缓存一致性问题和缓存穿透问题

--- backend/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ backend/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,13 +9,13 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -43,10 +43,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.ok(shop);
 
         }
+        // 判断命中的是否是空值
+        if (shopJson != null){
+            //返回一个错误信息
+            return Result.fail("店铺信息不存在!");
+        }
         //4.不存在，根据id查询数据库
         Shop shop = getById(id);
         //5.不存在，返回错误
         if (shop == null){
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+
+            //返回错误信息
             return Result.fail("店铺不存在！");
         }
 
@@ -56,5 +65,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //7.返回
 
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null){
+            return Result.fail("店铺id不能为空");
+        }
+
+        //1.更新数据库
+        updateById(shop);
+        //2.删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return null;
     }
 }
