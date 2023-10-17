d83c53ec017c6fd67f965e8d8f1bf14331dcc100
lilray <2313715475@qq.com>
2023-10-17 17:58:53 +0800

    封装Redis工具类

--- backend/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ backend/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,6 +9,8 @@ import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -36,17 +38,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private CacheClient cacheClient;
 
     @Override
     public Result queryById(Long id) {
         //缓存穿透
         //Shop shop = queryWithPassThrough(id);
+//        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY,id,Shop.class,this::getById,CACHE_SHOP_TTL,TimeUnit.MINUTES);
 
         //互斥锁解决缓存击穿
         //Shop shop = queryWithMutex(id);
 
         //逻辑过期解决缓存击穿
-        Shop shop = queryWithLogicalExpire(id);
+//        Shop shop = queryWithLogicalExpire(id);
+        Shop shop = cacheClient.queryWithLogicalExpire(CACHE_SHOP_KEY,id,Shop.class,this::getById,CACHE_SHOP_TTL,TimeUnit.MINUTES);
+
         if (shop == null){
             return Result.fail("店铺不存在！");
         }
--- backend/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ backend/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,7 +1,9 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.security.Provider;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private IShopService shopService;
+    @Resource
+    private CacheClient cacheClient;
     @Test
     public void testSaveShop() throws InterruptedException {
-        shopService.saveShop2Redis(1L,10L);
-        
+//        shopService.saveShop2Redis(1L,10L);
+        Shop shop = shopService.getById(1L);
+
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L,shop,10L, TimeUnit.SECONDS);
+
     }
 
 }
